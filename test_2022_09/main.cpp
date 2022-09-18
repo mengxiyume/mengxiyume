347fef881d5b88245e581c4a192588084eb797b6
mengxiyume <94777252+mengxiyume@users.noreply.github.com>
2022-09-18 22:10:23 +0800

    202209182210pm

--- test_2022_09/main.cpp
+++ test_2022_09/main.cpp
@@ -4,21 +4,72 @@
 
 int main()
 {
-	int sum = 0, val = 0;
-	int n = 0;
-	std::cout << "Enter a Val" << std::endl;
-	std::cin >> n;
 
-	for (val = 1; val <= n; val++)
+	//打印输入范围内所有整数
+	int A, B;
+	std::cin >> A >> B;
+	int Tmp = 0;
+	if (A > B)
 	{
-		sum += val;
-		std::cout << sum << " ";
+		while (B <= A)
+		{
+			std::cout << B++ << ' ';
+		}
+		std::cout << std::endl;
+	}
+	else if (A < B)
+	{
+		while (A <= B)
+		{
+			std::cout << A++ << ' ';
+		}
+		std::cout << std::endl;
+	}
+	else
+	{
+		std::cout << A << std::endl;
 	}
 
-	std::cout << "\nThe sum is " << sum << std::endl;
+	//用--打印10到0之间的所有整数
+	//int i = 10;
+	//while (i)
+	//{
+	//	std::cout << i-- << ' ';
+	//}
+	//std::cout << i << std::endl;
+
+	//打印50加到100的和
+	//int i = 50;
+	//int sum = 0;
+	//while (i++ <= 100)
+	//{
+	//	sum += i;
+	//	std::cout << sum << ' ';
+	//}
+	//std::cout << std::endl;
+
+	//std::cout << "The sum of 50 to 100 is " << sum << std::endl;
 
 	return 0;
 }
+
+//int main()
+//{
+//	int sum = 0, val = 0;
+//	int n = 0;
+//	std::cout << "Enter a Val" << std::endl;
+//	std::cin >> n;
+//
+//	for (val = 1; val <= n; val++)
+//	{
+//		sum += val;
+//		std::cout << sum << " ";
+//	}
+//
+//	std::cout << "\nThe sum is " << sum << std::endl;
+//
+//	return 0;
+//}
 
 //int main()
 //{
